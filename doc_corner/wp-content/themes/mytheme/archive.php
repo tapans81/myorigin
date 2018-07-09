1bb193e1e1963a338e7299be1b06bd38da6b577b
TKS <tapan.s81@gmail.com>
2018-07-09 17:05:02 +0530

    test commit 2

--- doc_corner/wp-content/themes/mytheme/archive.php
+++ doc_corner/wp-content/themes/mytheme/archive.php
@@ -54,7 +54,7 @@ echo "</div></div>";
         
       </div> -->
       <div class="modal-body">
-        <p>Downloads</p>
+        <p>File  Downloads</p>
        <table class="table">
         <thead>
           <tr>
